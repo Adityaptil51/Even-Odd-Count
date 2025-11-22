b55ed05d962cc841739899f54c054964d09c12ed
Adityaptil51 <adityaapatil51@gmail.com>
2025-11-22 12:59:45 +0530

    Update EvenOdd.py

--- EvenOdd.py
+++ EvenOdd.py
@@ -1,12 +1,20 @@
 import sys
 
-if len(sys.argv) == 2:
-    numbers = list(map(int, sys.argv[1].split(',')))
-    print("Numbers received from Jenkins parameter:", numbers)
+if len(sys.argv) != 2:
+    print("Usage: <NUMBERS>")
     sys.exit(1)
-else:
-    numbers = [1, 2, 3, 4, 5, 6]
-    print("No Jenkins parameter provided. Using default numbers:", numbers)
+
+try:
+    numbers = list(map(int, sys.argv[1].split(',')))
+except ValueError:
+    print("ERROR: Only integers allowed.")
+    sys.exit(1)
+
+# Check for 2 or 3 numbers
+if len(numbers) < 2 or len(numbers) > 3:
+    sys.exit(1)
+
+print("Numbers:", numbers)
 
 even_count = sum(1 for n in numbers if n % 2 == 0)
 odd_count = len(numbers) - even_count
