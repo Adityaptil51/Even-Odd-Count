994a5051a49a3305b1bb5bdbda0d213761e9aaa1
Adityaptil51 <adityaapatil51@gmail.com>
2025-11-22 13:04:38 +0530

    Update EvenOdd.py

--- EvenOdd.py
+++ EvenOdd.py
@@ -1,23 +1,19 @@
 import sys
 
-if len(sys.argv) != 2:
-    print("Usage: <NUMBERS>")
+if len(sys.argv) < 2:
+    print("Usage: python even_odd.py <num1> <num2> <num3> ...")
     sys.exit(1)
 
-try:
-    numbers = list(map(int, sys.argv[1].split(',')))
-except ValueError:
-    print("ERROR: Only integers allowed.")
-    sys.exit(1)
+numbers = list(map(int, sys.argv[1:]))
 
-# Check for 2 or 3 numbers
-if len(numbers) < 2 or len(numbers) > 3:
-    sys.exit(1)
+even_count = 0
+odd_count = 0
 
-print("Numbers:", numbers)
-
-even_count = sum(1 for n in numbers if n % 2 == 0)
-odd_count = len(numbers) - even_count
+for n in numbers:
+    if n % 2 == 0:
+        even_count += 1
+    else:
+        odd_count += 1
 
 print("Count of Even Numbers:", even_count)
 print("Count of Odd Numbers:", odd_count)
